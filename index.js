2527c4d0f4a2208aac8c20208d46ce11e6e286b4
Provashish Roy <provashishroy95@gmail.com>
2022-04-27 13:10:49 +0600

    get orders by user email

--- index.js
+++ index.js
@@ -50,13 +50,15 @@ async function run() {
             res.send(result);
         });
 
-        //order collection API
-        app.get('/orders', async (req, res) => {
-            const query = {}
+        //get order by email
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
             res.send(orders)
-        })
+        });
+
         app.post('/order', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
