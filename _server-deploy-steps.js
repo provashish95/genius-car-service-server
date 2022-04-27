7a12a09a5f0e5fdcda3315ba2056f9235dc686e2
Provashish Roy <provashishroy95@gmail.com>
2022-04-28 00:43:24 +0600

    added heroku

---
+++ _server-deploy-steps.js
@@ -0,0 +1,13 @@
+/**
+ * ----------------
+ * ONE TIME for pc
+ * ----------------
+ * 1.Create heroku account 
+ * 2.Verify email 
+ * 3.Install heroku CLI
+ * 4.heroku login  cmd of own cmd command not bash
+ * -------------------------
+ * For each project one time 
+ * -------------------------
+ * 1.heroku create
+ */
